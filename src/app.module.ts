19adccaec97ab5538699e5b12879c00deacdedb0
Bogdan Petrukovich <viorbrint@gmail.com>
2024-03-22 16:54:44 +0300

    delete folder server

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule, TestModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
